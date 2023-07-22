d062adffb3ed2825cc32815244830de096a8ce86
liuruizhou <liuruizhou@bilibili.com>
2023-07-22 13:10:38 +0800

    add

--- temp/client/client.go
+++ temp/client/client.go
@@ -1,21 +1,24 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	pb "google.golang.org/grpc/examples/features/proto/echo"
 	"google.golang.org/grpc/keepalive"
 )
 
 var addr = flag.String("addr", "localhost:50052", "the address to connect to")
 
 var kacp = keepalive.ClientParameters{
-	Time:                5 * time.Second, // send pings every 10 seconds if there is no activity
-	Timeout:             time.Second,     // wait 1 second for ping ack before considering the connection dead
-	PermitWithoutStream: false,           // send pings even without active streams
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+	PermitWithoutStream: true,             // send pings even without active streams
 }
 
 func main() {
@@ -27,15 +30,15 @@ func main() {
 	}
 	defer conn.Close()
 
-	// c := pb.NewEchoClient(conn)
+	c := pb.NewEchoClient(conn)
 
-	// ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
-	// defer cancel()
-	// fmt.Println("Performing unary request")
-	// res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "keepalive demo"})
-	// if err != nil {
-	// 	log.Fatalf("unexpected error from UnaryEcho: %v", err)
-	// }
-	// fmt.Println("RPC response:", res)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	defer cancel()
+	fmt.Println("Performing unary request")
+	res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "keepalive demo"})
+	if err != nil {
+		log.Fatalf("unexpected error from UnaryEcho: %v", err)
+	}
+	fmt.Println("RPC response:", res)
 	select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness.
 }
--- temp/server/server.go
+++ temp/server/server.go
@@ -9,24 +9,23 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
-
 	pb "google.golang.org/grpc/examples/features/proto/echo"
+	"google.golang.org/grpc/keepalive"
 )
 
 var port = flag.Int("port", 50052, "port number")
 
 var kaep = keepalive.EnforcementPolicy{
-	MinTime:             20 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
-	PermitWithoutStream: false,           // Allow pings even when there are no active streams
+	MinTime:             10 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
+	PermitWithoutStream: true,             // Allow pings even when there are no active streams
 }
 
 var kasp = keepalive.ServerParameters{
-	MaxConnectionIdle:     50 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-	MaxConnectionAge:      60 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
-	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
-	Time:                  80 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
-	Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
+	MaxConnectionIdle:     5000 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
+	MaxConnectionAge:      6000 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+	MaxConnectionAgeGrace: 5 * time.Second,    // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
+	Time:                  5 * time.Second,    // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+	Timeout:               1 * time.Second,    // Wait 1 second for the ping ack before assuming the connection is dead
 }
 
 // server implements EchoServer.
