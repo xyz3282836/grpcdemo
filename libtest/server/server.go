8664cb67ad516b4839f7ced1e3491aa6d179e741
liuruizhou <liuruizhou@bilibili.com>
2023-07-23 10:04:55 +0800

    add

--- libtest/server/server.go
+++ libtest/server/server.go
@@ -17,14 +17,14 @@ var port = flag.Int("port", 50052, "port number")
 
 var kaep = keepalive.EnforcementPolicy{
 	MinTime:             10 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
-	PermitWithoutStream: true,             // Allow pings even when there are no active streams
+	PermitWithoutStream: false,             // Allow pings even when there are no active streams
 }
 
 var kasp = keepalive.ServerParameters{
 	MaxConnectionIdle:     5000 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
 	MaxConnectionAge:      6000 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
 	MaxConnectionAgeGrace: 5 * time.Second,    // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
-	Time:                  5 * time.Second,    // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+	Time:                  15 * time.Second,    // Ping the client if it is idle for 5 seconds to ensure the connection is still active
 	Timeout:               1 * time.Second,    // Wait 1 second for the ping ack before assuming the connection is dead
 }
 
