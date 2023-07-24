675246d2e2d05d7602d50eafac142a0c35f21477
liuruizhou <liuruizhou@bilibili.com>
2023-07-24 17:24:11 +0800

    add

--- cmd/main.go
+++ cmd/main.go
@@ -12,15 +12,17 @@ import (
 	"grpcdemo/server"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthgrpc "google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/keepalive"
 )
 
 var kasp = keepalive.ServerParameters{
-	MaxConnectionIdle:     5000 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-	MaxConnectionAge:      6000 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
-	MaxConnectionAgeGrace: 5 * time.Second,    // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
-	Time:                  15 * time.Second,   // Ping the client if it is idle for 5 seconds to ensure the connection is still active
-	Timeout:               1 * time.Second,    // Wait 1 second for the ping ack before assuming the connection is dead
+	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
+	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
+	Time:                  20 * time.Second, // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+	Timeout:               5 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
 }
 
 func main() {
@@ -30,6 +32,9 @@ func main() {
 	grpcServer := grpc.NewServer(grpc.KeepaliveParams(kasp))
 	// 注册接口
 	v1.RegisterHelloServer(grpcServer, &server.HelloServer{})
+	health := health.NewServer()
+	healthgrpc.RegisterHealthServer(grpcServer, health)
+
 	// 启动
 	go grpcServer.Serve(ln)
 	log.Printf("grpc server start")
@@ -40,7 +45,8 @@ func main() {
 		s := <-c
 		switch s {
 		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
-			grpcServer.GracefulStop()
+			health.Shutdown()
+			grpcServer.Stop()
 			log.Printf("grpc server stop")
 			return
 		case syscall.SIGHUP:
--- grpcclient/normal/main.go
+++ grpcclient/normal/main.go
@@ -18,7 +18,7 @@ import (
 var kacp = keepalive.ClientParameters{
 	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
 	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
-	PermitWithoutStream: true,             // send pings even without active streams
+	PermitWithoutStream: false,            // send pings even without active streams
 }
 
 func main() {
