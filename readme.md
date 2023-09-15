af7463ed444df09617bd91c5f5915f8ae59000ab
liuruizhou <liuruizhou@bilibili.com>
2023-09-15 19:33:36 +0800

    add scan grpc api tool

--- readme.md
+++ readme.md
@@ -1,27 +1,45 @@
 # 生成proto
+
 有任何改动执行，绝对路径，换成自己机器的
+
 ## go
+
+```sh
 protoc --go_out=/Users/zhou/go/src/grpcdemo/ --go-grpc_out=/Users/zhou/go/src/grpcdemo/ api/v1/hello.proto
+```
+
 ### client diffproto
+
+```sh
 protoc --go_out=/Users/zhou/go/src/grpcdemo/grpcclient/protodiff --go-grpc_out=/Users/zhou/go/src/grpcdemo/grpcclient/protodiff grpcclient/protodiff/api/v1/hello.proto
+```
 
 ## gogo
+
+```sh
 protoc --gofast_out=plugins=grpc,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:/Users/zhou/go/src/grpcdemo/ api/v1/gogo/hello.proto
 protoc --gogofast_out=plugins=grpc,Mgoogle/protobuf/any.proto=github.com/gogo/protobuf/types:/Users/zhou/go/src/grpcdemo/ api/v1/gogo/hello.proto
+```
 
+## 启动grpc服务
 
-# 启动grpc服务
 启动grpc服务
 `go run cmd/gogo/main.go`
 `go run cmd/main.go`
 
 ## 查看grpc服务
+
 查看grpc服务进程
 `lsof -i :9000`
 
 ## 客户端请求
+
 模拟grpc请求，以http的方式
 `go run httpclient/main.go`
 grpc客户端请求
 `go run grpcclient/normal/gogo/main.go`
 `go run grpcclient/normal/main.go`
+
+## 扫描grpc server api list
+
+`go run main.go -ip 10.150.255.153`
--- tool/main.go
+++ tool/main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
+	"fmt"
+	"strings"
 	"time"
 
-	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/keepalive"
 
-	grpcrf "github.com/jhump/protoreflect/grpcreflect"
+	"log"
+	// https://github.com/gogo/protobuf/protoc-gen-gogo
+	"github.com/golang/protobuf/proto"
+	dpb "github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"google.golang.org/grpc"
+	rpb "google.golang.org/grpc/reflection/grpc_reflection_v1alpha"
 )
+
+type ErrorResponse struct {
+	*rpb.ServerReflectionResponse_ErrorResponse
+}
+
+func (e *ErrorResponse) Error() string {
+	return fmt.Sprintf("error_code:%d  error_message:%s", e.ErrorResponse.ErrorCode, e.ErrorResponse.ErrorMessage)
+}
 
 var kacp = keepalive.ClientParameters{
 	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
@@ -20,39 +33,109 @@ var kacp = keepalive.ClientParameters{
 }
 
 func main() {
-	var addr string
-	flag.StringVar(&addr, "addr", "10.150.134.199:9000", "-addr 127.0.0.1:9000")
+	var ip string
+	var port string
+	flag.StringVar(&ip, "ip", "10.150.124.121", "-ip 127.0.0.1")
+	flag.StringVar(&port, "port", "9000", "-port 9000")
 	flag.Parse()
 
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithKeepaliveParams(kacp))
-	conn, err := grpc.Dial(addr, opts...)
+	// 建联grpc服务
+	conn, err := grpc.Dial(ip+":"+port, opts...)
 	if err != nil {
 		log.Printf("grpc.Dial fail %v", err)
 		panic("grpc dial fail")
 	}
 	defer conn.Close()
 
-	TestGetRelectApiList(conn)
+	reflectionList(context.TODO(), conn)
 }
 
-func TestGetRelectApiList(conn *grpc.ClientConn) {
-	c := grpcrf.NewClientAuto(context.TODO(), conn)
-	slist, err := c.ListServices()
+func reflectionList(ctx context.Context, conn *grpc.ClientConn) {
+	// 获取grpc服务信息
+	client := rpb.NewServerReflectionClient(conn)
+	stream, err := client.ServerReflectionInfo(ctx, grpc.WaitForReady(true))
 	if err != nil {
-		log.Printf("err %v", err)
+		log.Printf("ServerReflectionInfo err %v", err)
 		return
 	}
-	log.Printf("api len %d", len(slist))
-	for _, e := range slist {
-		log.Printf("service name is %s", e)
-		d, err := c.ResolveService(e)
-		if err != nil {
-			log.Printf("ResolveService err %v", err)
-			return
+	services, err := listServices(ctx, stream)
+	if err != nil {
+		log.Printf("listServices err %v", err)
+		return
+	}
+	var methods []string
+	log.Printf("v2 api service len %d", len(services.ListServicesResponse.Service))
+	for index, v := range services.ListServicesResponse.Service {
+		log.Println()
+		log.Printf("v2 service name is %s and index is %d", v.Name, index+1)
+		info, e := getServiceInfo(ctx, stream, v.Name)
+		if e != nil {
+			log.Printf("service name %s 需要将它所在proto文件通过非gogo工具重新生成一次 err %v", v.Name, e)
+			continue
 		}
-		for _, ee := range d.GetMethods() {
-			log.Printf("api is /%s/%s", e, ee.GetName())
+		for _, content := range info.FileDescriptorResponse.FileDescriptorProto {
+			m := dpb.FileDescriptorProto{}
+			if err = proto.Unmarshal(content, &m); err != nil {
+				log.Printf("unmarshal file descriptor err %v service name is %s", err, m.Name)
+				continue
+			}
+			for _, s := range m.Service {
+				strArr := strings.Split(v.Name, ".")
+				if strArr[len(strArr)-1] != s.GetName() {
+					continue
+				}
+				log.Printf("-- v2 service name is %s and method len is %d", s.GetName(), len(s.Method))
+				for _, method := range s.Method {
+					methods = append(methods, fmt.Sprintf("/%s/%s", v.Name, method.GetName()))
+					log.Printf("-- -- v2 api is /%s/%s", v.Name, method.GetName())
+				}
+			}
 		}
 	}
+	log.Println()
+	log.Printf("all api num is %d", len(methods))
+	return
+}
+
+func listServices(ctx context.Context, stream rpb.ServerReflection_ServerReflectionInfoClient) (res *rpb.ServerReflectionResponse_ListServicesResponse, err error) {
+	err = stream.Send(&rpb.ServerReflectionRequest{
+		MessageRequest: &rpb.ServerReflectionRequest_ListServices{},
+	})
+	if err != nil {
+		return
+	}
+	resp, err := stream.Recv()
+	if err != nil {
+		log.Printf("listServices err %v", err)
+		return
+	}
+	res, ok := resp.MessageResponse.(*rpb.ServerReflectionResponse_ListServicesResponse)
+	if !ok {
+		log.Printf("listServices MessageResponse is not list")
+		err = fmt.Errorf("listServices reponse not valid")
+		return
+	}
+	return
+}
+
+func getServiceInfo(ctx context.Context, stream rpb.ServerReflection_ServerReflectionInfoClient, serviceName string) (res *rpb.ServerReflectionResponse_FileDescriptorResponse, err error) {
+	err = stream.Send(&rpb.ServerReflectionRequest{
+		MessageRequest: &rpb.ServerReflectionRequest_FileContainingSymbol{FileContainingSymbol: serviceName},
+	})
+	if err != nil {
+		return
+	}
+	resp, err := stream.Recv()
+	if err != nil {
+		log.Printf("getServiceInfo err %v", err)
+		return
+	}
+	res, ok := resp.MessageResponse.(*rpb.ServerReflectionResponse_FileDescriptorResponse)
+	if !ok {
+		err = fmt.Errorf("getServiceInfo reponse not valid")
+		return
+	}
+	return
 }
